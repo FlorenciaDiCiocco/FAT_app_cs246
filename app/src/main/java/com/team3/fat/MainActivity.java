c613eaddd28e4a915f12f8c3a0d6a9ec4b5e9752
florencia.di.ciocco <florencia.di.ciocco@gmail.com>
2021-02-18 12:36:31 -0300

    Customized the listview. Now I will make it more to our app.

--- app/src/main/java/com/team3/fat/MainActivity.java
+++ app/src/main/java/com/team3/fat/MainActivity.java
@@ -1,20 +1,47 @@
 package com.team3.fat;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
-import android.widget.ArrayAdapter;import android.widget.ListView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity
-{
-    // Array of strings...
-    ListView simpleList;
-    String countryList[] = {"India", "China", "australia", "Portugle", "America", "NewZealand"};
+import java.util.ArrayList;
 
-    @Override   protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);      setContentView(R.layout.activity_main);
-        simpleList = (ListView)findViewById(R.id.simpleListView);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_listview, R.id.textView, countryList);
-        simpleList.setAdapter(arrayAdapter);
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ArrayList userList = getListData();
+        final ListView lv = (ListView) findViewById(R.id.user_list);
+        lv.setAdapter(new CustomListAdapter(this, userList));
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                ListItem user = (ListItem) lv.getItemAtPosition(position);
+                Toast.makeText(MainActivity.this, "Selected :" + " " + user.getName()+", "+ user.getLocation(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private ArrayList getListData() {
+        ArrayList<ListItem> results = new ArrayList<>();
+        ListItem user1 = new ListItem();
+        user1.setName("Suresh Dasari");
+        user1.setDesignation("Team Leader");
+        user1.setLocation("Hyderabad");
+        results.add(user1);
+        ListItem user2 = new ListItem();
+        user2.setName("Rohini Alavala");
+        user2.setDesignation("Agricultural Officer");
+        user2.setLocation("Guntur");
+        results.add(user2);
+        ListItem user3 = new ListItem();
+        user3.setName("Trishika Dasari");
+        user3.setDesignation("Charted Accountant");
+        user3.setLocation("Guntur");
+        results.add(user3);
+        return results;
     }
 }
