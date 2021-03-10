04e54da52c78b862247ab67cf9ee5e06aea04b05
Dylan Havens <dsh95@byui.edu>
2021-03-10 13:44:08 -0700

    I moved the Login to the MainActivity and what was in the MainActivity to DisplayHome

--- app/src/main/java/com/team3/fat/MainActivity.java
+++ app/src/main/java/com/team3/fat/MainActivity.java
@@ -1,46 +1,93 @@
 package com.team3.fat;
 
+import android.content.Intent;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-import java.util.ArrayList;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth Auth;
+    EditText userEmail, userPassword;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ArrayList userList = getListData();
-        final ListView lv = (ListView) findViewById(R.id.user_list);
-        lv.setAdapter(new CustomListAdapter(this, userList));
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        setContentView(R.layout.activity_login);
+
+        userEmail = findViewById(R.id.username);
+        userPassword = findViewById(R.id.password);
+        Auth = FirebaseAuth.getInstance();
+
+        Button newUser = findViewById(R.id.newUser);
+        newUser.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
-                ListItem user = (ListItem) lv.getItemAtPosition(position);
-                Toast.makeText(MainActivity.this, "Selected :" + " " + user.getWeight()+", "+ user.getDate(), Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                open_registerUser();
             }
         });
+
+        Button forgotPass = findViewById(R.id.forgotPass);
+        forgotPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                open_forgotPassword();
+            }
+        });
+
+
+        Button login = findViewById(R.id.Login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String stringEmail = userEmail.getText().toString().trim();
+                String stringPassword = userPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(stringEmail)){
+                    Toast.makeText(MainActivity.this, "Please enter a Email.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(stringPassword)){
+                    Toast.makeText(MainActivity.this,"Please enter a Password.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Auth.signInWithEmailAndPassword(stringEmail, stringPassword).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            startActivity(new Intent(getApplicationContext(), DisplayHome.class));  //change this to the main activity after login and also change all loginMain to MainActivity
+                        }
+                        else{
+                            Toast.makeText(MainActivity.this, "Login Failed, Please try again", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+    };
+
+    public void open_registerUser(){
+        Intent intent = new Intent(this, registerUser.class);
+        startActivity(intent);
     }
-    private ArrayList getListData() {
-        //This function is to add elements to the list that will be printed
-        float number = 88.5f;
-        ArrayList<ListItem> results = new ArrayList<>();
-        ListItem user1 = new ListItem();
-        user1.setDate("december");
-        user1.setWeight(number);
-        results.add(user1);
-        ListItem user2 = new ListItem();
-        user2.setDate("december");
-        user2.setWeight(number);
-        results.add(user2);
-        ListItem user3 = new ListItem();
-        user3.setDate("december");
-        user3.setWeight(number);
-        results.add(user3);
-        return results;
+
+    public void open_forgotPassword(){
+        Intent intent = new Intent(this, forgotPassword.class);
+        startActivity(intent);
     }
-}
+};
