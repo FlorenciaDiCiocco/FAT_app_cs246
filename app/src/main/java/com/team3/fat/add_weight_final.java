761497caa7cbdffa544a747ae23c55bf5b74e2f1
Lucy Davidson <dav18033@byui.edu>
2021-03-19 17:57:31 -0400

    Adding the weight activity!!

--- app/src/main/java/com/team3/fat/add_weight_final.java
+++ app/src/main/java/com/team3/fat/add_weight_final.java
@@ -3,12 +3,27 @@ package com.team3.fat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
+import android.widget.EditText;
 
 public class add_weight_final extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_add_weight_final);
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_weight_final);
+        setContentView(R.layout.activity_add_weight);
+
+        EditText editText = (EditText) findViewById(R.id.editText);
+        addWeightKeyboard keyboard = (addWeightKeyboard) findViewById(R.id.keyboard);
+        editText.setRawInputType(InputType.TYPE_CLASS_TEXT);
+        editText.setTextIsSelectable(true);
+
+        InputConnection ic = editText.onCreateInputConnection(new EditorInfo());
+        keyboard.setInputConnection(ic);
     }
 }
